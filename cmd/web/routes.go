43f74f273dcbfe3fe4db4320a838f42db112762d
williamneo <williamneokh@gmail.com>
2022-03-02 10:04:20 +0800

    add static images and update routes fileServer + mux.Handle

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -13,7 +13,11 @@ func routes(app *config.Appconfig) http.Handler {
 	mux.Use(middleware.Recoverer)
 	mux.Use(NoSurf)
 	mux.Use(SessionLoad)
+
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 	return mux
 }
